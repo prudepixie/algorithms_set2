97f20984cf68e32cf4626988bc2c7df84ef7f50a
Wendy Hsu <wndyhsu@gmail.com>
2014-03-30 11:30:17 -0500

    completed my_hash

--- my_hash/my_hash.rb
+++ my_hash/my_hash.rb
@@ -1,13 +1,21 @@
 class MyHash
+  attr_accessor :array
   def initialize
+
+    @array = Array.new(64)
   end
 
   def find_index(key)
+    key.hash%8
   end
 
   def []=(key, value)
+    index = find_index(key)
+    @array[index]= value
   end
 
   def [](key)
+    index = find_index(key)
+    @array[index]
   end
 end

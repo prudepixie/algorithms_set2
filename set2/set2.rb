060b5b92470f3398f3696c6d9393da7504f3156b
Wendy Hsu <wndyhsu@gmail.com>
2014-03-30 12:23:02 -0500

    completed find_mode

--- set2/set2.rb
+++ set2/set2.rb
@@ -1,4 +1,33 @@
 module Set2
-  def self.find_intersection
+  def self.find_intersection(a1, a2, a3)
+
+    if a1&a2&a3 == a3 && a1.empty? && a2.empty? && a3.empty?
+       []
+    else
+      a1&a2&a3
+    end
   end
+
+  def self.find_first_dup(a1)
+    dup= a1.detect{ |e| a1.count(e) > 1 }
+
+    if dup ==false
+      nil
+    else
+      dup
+  end
+end
+
+  def self.find_mode(a1)
+    dup = a1.detect{ |e| a1.count(e) > 1 }
+    dup_num = a1.count(dup)
+    dup_dup_num = a1.detect {|f| a1.count(f)>dup_num}
+    if a1.empty?
+      nil
+    elsif dup_dup_num == nil
+      dup
+    else
+      dup_dup_num
+    end
+end
 end
--- set2/spec/set2_spec.rb
+++ set2/spec/set2_spec.rb
@@ -5,40 +5,40 @@ require_relative '../set2.rb'
 
 describe "Set2" do
   describe ".find_intersection" do
-    xit "will return empty array for 3 empty arrays" do
+    it "will return empty array for 3 empty arrays" do
       expect(Set2.find_intersection([],[],[])).to eq([])
     end
 
-    xit "will return empty array for 3 empty arrays with no common elements" do
+    it "will return empty array for 3 empty arrays with no common elements" do
       expect(Set2.find_intersection([5],[6],[7])).to eq([])
       expect(Set2.find_intersection([5,10, 15, 20],[6, 12, 18, 24],[7, 14, 21, 28])).to eq([])
       expect(Set2.find_intersection([15, 20, 5, 10],[12, 24, 18, 6],[7, 14, 28, 21])).to eq([])
     end
 
-    xit "will return an array with 1 element if that's the only common one" do
+    it "will return an array with 1 element if that's the only common one" do
       expect(Set2.find_intersection([7],[7],[7])).to eq([7])
       expect(Set2.find_intersection([15, 20, 7, 5, 10],[12, 7, 24, 18, 6],[7, 14, 28, 21])).to eq([7])
     end
 
-    xit "will return an array with all intersecting values" do
+    it "will return an array with all intersecting values" do
       intersection = Set2.find_intersection([24, 15, 20, 7, 5, 10],[12, 7, 24, 18, 6],[7, 24, 14, 28, 21])
       expect(intersection.include?(7)).to eq(true)
       expect(intersection.include?(24)).to eq(true)
       expect(intersection.size).to eq(2)
     end
 
-    xit "will not include values that are only present in 2 arrays" do
+    it "will not include values that are only present in 2 arrays" do
       expect(Set2.find_intersection([10],[10],[7])).to eq([])
     end
 
   end
 
   describe ".find_first_dup" do
-    xit "will return nil if there are no duplicates" do
+    it "will return nil if there are no duplicates" do
       expect(Set2.find_first_dup([1, 2, 3, 4, 5])).to eq(nil)
     end
 
-    xit "will return the first duplicate in the array" do
+    it "will return the first duplicate in the array" do
       expect(Set2.find_first_dup([1, 1])).to eq(1)
       expect(Set2.find_first_dup([-1, -1])).to eq(-1)
       expect(Set2.find_first_dup([0, 7, -1,8, 21, 2, -1])).to eq(-1)
@@ -47,11 +47,11 @@ describe "Set2" do
   end
 
   describe ".find_mode" do
-    xit "will return nil if there are no elements" do
+    it "will return nil if there are no elements" do
       expect(Set2.find_mode([])).to eq(nil)
     end
 
-    xit "will return the mode of the array" do
+    it "will return the mode of the array" do
       expect(Set2.find_mode([1, 1])).to eq(1)
       expect(Set2.find_mode([-1, -1])).to eq(-1)
       expect(Set2.find_mode([5, 10, 15, 20, 5, 10, 10, 5, 10])).to eq(10)
